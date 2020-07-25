c24cb884afd757786fa9f25b2039e153fc782ea1
ThamanR <58350600+ThamanR@users.noreply.github.com>
2020-07-26 00:22:10 +0100

    Update controllers2.py
    Changes made to the code according to the undertaken project with added remote controller connections defined

--- examples/controllers2.py
+++ examples/controllers2.py
@@ -20,33 +20,39 @@ from mininet.log import setLogLevel, info
 def multiControllerNet():
     "Create a network from semi-scratch with multiple controllers."
 
-    net = Mininet( controller=Controller, switch=OVSSwitch )
+   net = Mininet( controller=RemoteController, switch=OVSSwitch)
 
-    info( "*** Creating (reference) controllers\n" )
-    c1 = net.addController( 'c1', port=6633 )
-    c2 = net.addController( 'c2', port=6634 )
+    info( "*** Creating remote controllers\n" )
+    c1 = net.addController( 'c1', ip='172.17.0.2', port=6633, protocols=["OpenFlow13"] )
+   
 
     info( "*** Creating switches\n" )
-    s1 = net.addSwitch( 's1' )
-    s2 = net.addSwitch( 's2' )
+    s1 = net.addSwitch( 's1',  protocols=["OpenFlow13"]  )
+    s2 = net.addSwitch( 's2',  protocols=["OpenFlow13"]  )
+    s3 = net.addSwitch( 's3',  protocols=["OpenFlow13"]  )
+    
 
     info( "*** Creating hosts\n" )
-    hosts1 = [ net.addHost( 'h%d' % n ) for n in ( 3, 4 ) ]
-    hosts2 = [ net.addHost( 'h%d' % n ) for n in ( 5, 6 ) ]
+    hostsSwitch1 = [ net.addHost( 'h%d' % n ) for n in [1, 2] ] #add hosts named h1,h2
+    hostsSwitch2 = [ net.addHost( 'h%d' % n ) for n in [3, 4] ] #add hosts named h3,h4
+    hostsSwitch3 = [ net.addHost( 'h%d' % n ) for n in [5, 6] ] #add hosts named h5,h6
 
     info( "*** Creating links\n" )
-    for h in hosts1:
+    for h in hostsSwitch1:
         net.addLink( s1, h )
-    for h in hosts2:
+    for h in hostsSwitch2:
         net.addLink( s2, h )
+    for h in hostsSwitch3:
+        net.addLink( s3, h )
     net.addLink( s1, s2 )
+    net.addLink( s2, s3 )
+    net.addLink( s3, s1 )
 
-    info( "*** Starting network\n" )
+    info( "*** Starting network\n" ) #Connect switches to remote controller
     net.build()
-    c1.start()
-    c2.start()
     s1.start( [ c1 ] )
-    s2.start( [ c2 ] )
+    s2.start( [ c1 ] )
+    s3.start( [ c1 ] )
 
     info( "*** Testing network\n" )
     net.pingAll()
@@ -54,9 +60,6 @@ def multiControllerNet():
     info( "*** Running CLI\n" )
     CLI( net )
 
-    info( "*** Stopping network\n" )
-    net.stop()
-
 if __name__ == '__main__':
     setLogLevel( 'info' )  # for CLI output
-    multiControllerNet()
+    remoteControllerNet()
